06b2d37af4990ff33b9ed9ca794fb70364934139
Ayla Myers <bridgs.dev@gmail.com>
2019-06-10 15:33:58 -0400

    wip

---
+++ entity-performance-comparison.lua
@@ -0,0 +1,45 @@
+local entities = {}
+
+function love.load()
+  for i = 1, 1150 * 72 do
+    table.insert(entities, {
+      x = math.random(0, 380),
+      y = math.random(0, 380),
+      width = 20,
+      height = 20,
+      vx = math.random(-100, 100),
+      vy = math.random(-100, 100),
+      color = { math.random(), math.random(), math.random() }
+    })
+  end
+end
+
+function love.update(dt)
+  for _, entity in ipairs(entities) do
+    entity.x = entity.x + entity.vx * dt
+    entity.y = entity.y + entity.vy * dt
+    if entity.x < 0 then
+      entity.x, entity.vx = 0, math.abs(entity.vx)
+    elseif entity.x > 400 - entity.width then
+      entity.x, entity.vx = 400 - entity.width, -math.abs(entity.vx)
+    end
+    if entity.y < 0 then
+      entity.y, entity.vy = 0, math.abs(entity.vy)
+    elseif entity.y > 400 - entity.height then
+      entity.y, entity.vy = 400 - entity.height, -math.abs(entity.vy)
+    end
+  end
+end
+
+function love.draw()
+  -- Clear the screen
+  love.graphics.clear(0.1, 0.1, 0.1)
+  -- Draw each entity
+  for _, entity in ipairs(entities) do
+    love.graphics.setColor(entity.color)
+    love.graphics.rectangle('fill', entity.x, entity.y, entity.width, entity.height)
+  end
+  -- Draw the frame rate
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print('Frames per second: '..tostring(love.timer.getFPS()), 10, 10)
+end
---
+++ entity-performance-example.lua
@@ -0,0 +1,60 @@
+-- Load simulsim as a dependency (you should use a url for a specific commit)
+local simulsim = require 'simulsim'
+
+local game = simulsim.defineGame()
+function game.update(self, dt)
+  for _, entity in ipairs(self.entities) do
+    entity.x = entity.x + entity.vx * dt
+    entity.y = entity.y + entity.vy * dt
+    if entity.x < 0 then
+      entity.x, entity.vx = 0, math.abs(entity.vx)
+    elseif entity.x > 400 - entity.width then
+      entity.x, entity.vx = 400 - entity.width, -math.abs(entity.vx)
+    end
+    if entity.y < 0 then
+      entity.y, entity.vy = 0, math.abs(entity.vy)
+    elseif entity.y > 400 - entity.height then
+      entity.y, entity.vy = 400 - entity.height, -math.abs(entity.vy)
+    end
+  end
+end
+function game.handleEvent(self, eventType, eventData)
+  if eventType == 'spawn-critter' then
+    self:spawnEntity({
+      x = eventData.x,
+      y = eventData.y,
+      width = 20,
+      height = 20,
+      vx = eventData.vx,
+      vy = eventData.vy,
+      color = eventData.color
+    })
+  end
+end
+
+local network, server, client = simulsim.createGameNetwork(game, { mode = 'development' })
+
+function server.load()
+  for i = 1, 1150 do
+    server.fireEvent('spawn-critter', {
+      x = math.random(0, 380),
+      y = math.random(0, 380),
+      vx = math.random(-100, 100),
+      vy = math.random(-100, 100),
+      color = { math.random(), math.random(), math.random() }
+    })
+  end
+end
+
+function client.draw()
+  -- Clear the screen
+  love.graphics.clear(0.1, 0.1, 0.1)
+  -- Draw each entity
+  for _, entity in ipairs(client.game.entities) do
+    love.graphics.setColor(entity.color)
+    love.graphics.rectangle('fill', entity.x, entity.y, entity.width, entity.height)
+  end
+  -- Draw the frame rate
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.print('Frames per second: '..tostring(love.timer.getFPS()), 10, 10)
+end
