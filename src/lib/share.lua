6ce85976c13545613810677af67f2f2fc1cc4e2d
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-06-28 18:35:50 -0700

    Update 'share.lua' again

--- src/lib/share.lua
+++ src/lib/share.lua
@@ -1,3 +1,3 @@
-local cs = require 'https://raw.githubusercontent.com/castle-games/share.lua/b6f2d80a365b3f73e71fbd4b0d001b75d15c3149/cs.lua'
+local cs = require 'https://raw.githubusercontent.com/castle-games/share.lua/623c500de3cbfd544db9e6f2bdcd03b5b7e6f377/cs.lua'
 
 return cs
